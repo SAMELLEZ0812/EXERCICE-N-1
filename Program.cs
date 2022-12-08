9e49ae59d779c1ca626d95a0e9053c752c009401
Chmouel <Chmouel@MAURICE>
2022-12-08 16:41:43 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * PROGRAMATION DE NOM
+ * FAIT PAR CHMOUEL ELLEZAM
+ * DATE:6/12/2022
+ */
+using System;
+
+
+namespace EXERCICE_N_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaration 
+            string prenom = " Doron";
+            string nom = "IFERGAN";
+            int age = 35;
+
+            //affichage du message personalisé
+            Console.WriteLine("Bonjour"   + prenom + " " + nom + " vous avez " +age+"ans  " );
+
+
+            Console.ReadLine();
+
+        
+        }
+        
+
+
+    }
+}
